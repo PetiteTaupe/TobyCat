2edebfd0641b6932dbaef0a85069f4afd0595b6e
Alexis Bechir <70131081+PetiteTaupe@users.noreply.github.com>
2022-03-30 23:10:24 +0200

    image aléatoire en tapant !cat

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const client = new Client({
 })
 const config = require('./config.json')
 
+const _ = require('lodash')
+const randomResponse = [ "https://imgur.com/d6BSgMS" , "https://imgur.com/GZ8hObx" , "https://imgur.com/lo0HqdX",
+                         "https://imgur.com/lo0HqdX" , "https://imgur.com/gh8i9QC" , "https://imgur.com/nVIGIJO",
+                         "https://imgur.com/VgdARTk" , "https://imgur.com/DaY8PcB" , "https://imgur.com/Ao9Lu4t",
+                         "https://imgur.com/aNDUcF2" , "https://imgur.com/JXzgI7y" , "https://imgur.com/BZzwABw"]
+
 client.on('messageCreate', (message) => {
     if (message.content.startsWith('!')) {
         let command = message.content.split(' ')[0].replace('!', '')
         console.log(command)
-        if (command === 'cat') {
+        if (command === 'miaou') {
             message.reply('miaou')
+        }
+        if (command === 'cat') {
+            message.reply(_.sample(randomResponse))
         }
     }
 })
