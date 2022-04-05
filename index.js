17a0f9120111bcf0c78b1e47f7b311678998b39c
Alexis Bechir <70131081+PetiteTaupe@users.noreply.github.com>
2022-04-05 16:29:54 +0200

    ub v2

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const {Client , MessageEmbed} = require('discord.js')
-
-const fs = require('fs');
-const path = require('path');
+const {Client, MessageEmbed, MessageAttachment} = require('discord.js')
 const config = require('./config.json')
 const axios = require('axios')
 const _ = require('lodash')
+const Canvas = require('canvas')
 
 const client = new Client({
     intents: ['DIRECT_MESSAGES', 'DIRECT_MESSAGE_REACTIONS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILDS'],
     partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 })
 
-const randomPost = ["Jungle", "ADC", "Support", "Top", "Mid"]
+const randomPost = ["Jungle", "Bot", "Support", "Top", "Mid"]
 
 let champions = null;
+let runes = null;
 
-client.on('messageCreate', (message) => {
-    if (message.content.startsWith('!')) {
-        let command = message.content.split(' ')[0].replace('!', '')
+client.login(config.token)
 
+client.on('ready', () => {
+    console.log('Bot Ready')
+    client.user.setActivity({type: 'PLAYING', name: 'casser de la vaisselle'})
+    getDataFromAPI()
+    setInterval(getDataFromAPI, 1000 * 60 * 60 * 6)
+})
+
+client.on('messageCreate', async (message) => {
+    if (message.content.startsWith(config.prefix)) {
+        let command = message.content.split(' ')[0].replace(config.prefix, '')
         console.log(command)
 
         switch (command) {
@@ -35,53 +42,120 @@ client.on('messageCreate', (message) => {
                     })
                 break;
             case 'ub':
-                //newList[randomId].name
-
                 let championsAsArray = Object.values(champions)
-
-                let randomChamp = _.sample(championsAsArray);
-
+                let randomChamp = _.sample(championsAsArray)
                 let randomChampName = randomChamp.name
                 let randomChampId = randomChamp.id
 
+                let runesAsArray = Object.values(runes)
+
+                let randomMajorFamilyRune = _.sample(runesAsArray)
+                let randomMajorFamilyRuneName = randomMajorFamilyRune.name
+                let randomMajorRune = _.sample(randomMajorFamilyRune.runes)
+                let randomMajorRuneName = randomMajorRune.name
+
+                let randomSecondaryFamilyRune
+                let randomSecondaryFamilyRuneName
+
+                do {
+                    randomSecondaryFamilyRune = _.sample(runesAsArray)
+                    randomSecondaryFamilyRuneName = randomSecondaryFamilyRune.name
+                } while (randomMajorFamilyRuneName === randomSecondaryFamilyRuneName)
+
+                let post = _.sample(randomPost)
+
+                /*
+                const canvas = Canvas.createCanvas(700, 700)
+                const ctx = canvas.getContext('2d')
+
+                Canvas.loadImage('http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' + randomChampId + '_0.jpg').then(image => {
+                    ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+                })
+
+                /*Canvas.loadImage('img/Position_Diamond-' + post + ".png").then(image => {
+                    ctx.drawImage(image, 0, 0, 100, 100)
+                })*/
+
+                //const attachment = new MessageAttachment(canvas.toBuffer(), 'ub.png');
+
+                //'https://ddragon.canisback.com/img/' + randomMajorRune.icon
+                //'https://ddragon.canisback.com/img/' + randomSecondaryFamilyRune.icon
+
+                //message.reply({ files: [attachment] })
+
+                const canvas = Canvas.createCanvas(960, 540);
+                const context = canvas.getContext('2d');
+
+                const background = await Canvas.loadImage('http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' + randomChampId + '_0.jpg');
+
+                // This uses the canvas dimensions to stretch the image onto the entire canvas
+                context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+                const imgBanner = await Canvas.loadImage('img/banner.png');
+
+                // Draw a shape onto the main canvas
+                context.drawImage(imgBanner, 10, 0, 200, 500);
+
+                const imgPost = await Canvas.loadImage('img/Position_Diamond-' + post + '.png');
+
+                // Draw a shape onto the main canvas
+                context.drawImage(imgPost, 60, 50, 100, 100);
+
+                const imgMajorRune = await Canvas.loadImage('https://ddragon.canisback.com/img/' + randomMajorRune.icon);
+
+                // Draw a shape onto the main canvas
+                context.drawImage(imgMajorRune, 25, 175, 100, 100);
+
+                const imgSecondaryRune = await Canvas.loadImage('https://ddragon.canisback.com/img/' + randomSecondaryFamilyRune.icon);
+
+                // Draw a shape onto the main canvas
+                context.drawImage(imgSecondaryRune, 125, 200, 50, 50);
+
+                // Use the helpful Attachment class structure to process the file for you
+                const attachment = new MessageAttachment(canvas.toBuffer(), 'ub.png');
+
                 const ultimate_bravery = new MessageEmbed()
                     .setColor('#7289D9')
-                    .setTitle('Ultimate Bravery')
-                    .setAuthor({name: 'TobyCat', iconURL: 'https://i.imgur.com/SHdyR65.png'})
-                    .setDescription('Voici ta prochaine game de la League des Croquettes, GOOD LUCK & HAVE FUN!')
-                    .addFields(
-                        {name: 'Ton Champion', value: randomChampName},
-                        {name: 'Ton rôle', value: _.sample(randomPost)},
-                    )
-                    .setImage('http://ddragon.leagueoflegends.com/cdn/12.6.1/img/champion/' + randomChampId + '.png')
+                    //.setTitle('Ultimate Bravery')
+                    .setAuthor({name: message.member.nickname, iconURL: message.author.avatarURL()})
+                    .setDescription('Voici ton ultimate bravery, GOOD LUCK & HAVE FUN!😼🎲😹')
+                    .addFields([
+                        {name: 'Ton Champion', value: randomChampName, inline: true},
+                        {name: 'Ton rôle', value: post, inline: true},
+                        {
+                            name: 'Tes runes',
+                            value: randomMajorRuneName + " | " + randomSecondaryFamilyRuneName,
+                            inline: true
+                        },
+                    ])
                     .setFooter({text: 'Powered by Toby The Fucking Cat'});
 
 
-                message.reply({embeds: [ultimate_bravery]})
+                message.reply({embeds: [ultimate_bravery], files: [attachment]})
                 break;
             default:
                 message.reply('che ne connais pas cha! 😾')
-//message.reply({files: ["img/error.png"]});
+                //message.reply({files: ["img/error.png"]});
                 break;
         }
-
     }
 })
 
-function getChampionsFromAPI() {
-    axios.get('http://ddragon.leagueoflegends.com/cdn/' + config.currentPatch + '/data/en_US/champion.json')
+function getDataFromAPI() {
+    axios.get('http://ddragon.leagueoflegends.com/cdn/' + config.currentPatch + '/data/fr_FR/champion.json')
         .then(res => {
-            fs.writeFileSync(path.resolve(__dirname, 'champion.json'), JSON.stringify(res.data.data));
+            champions = res.data.data
+        })
+    axios.get('http://ddragon.leagueoflegends.com/cdn/' + config.currentPatch + '/data/fr_FR/runesReforged.json')
+        .then(res => {
+            let data = res.data
 
-            champions = require('./champion.json')
+            data.forEach(runeType => {
+                runeType.runes = runeType.slots[0].runes
+                delete runeType.slots
+            })
+            runes = data;
         })
 }
 
-client.on('ready', () => {
-    console.log('Bot Ready')
-    client.user.setActivity({type: 'PLAYING', name: 'arbre à chat'})
-    getChampionsFromAPI()
-    setInterval(getChampionsFromAPI, 1000 * 60 * 60 * 6)
-})
 
-client.login(config.token)
